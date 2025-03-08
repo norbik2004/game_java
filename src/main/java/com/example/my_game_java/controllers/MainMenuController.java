11922c489b38887f49f5986ece63a330e88f4c15
Norbert <kieler103@gmail.com>
2025-03-08 10:27:15 +0100

    #main menu

--- src/main/java/com/example/my_game_java/controllers/MainMenuController.java
+++ src/main/java/com/example/my_game_java/controllers/MainMenuController.java
@@ -8,7 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,16 +23,27 @@ public class MainMenuController {
     private ImageView imageView;
 
     @FXML
+    private Label logo;
+
+    @FXML
+    private Button start_game;
+
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
     public void initialize() {
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/photos/main_menu.jpg")));
         imageView.setImage(image);
+        logo.setText("MAIN MENU");
+        start_game.setText("NEW GAME");
 
         imageView.setOpacity(0);
 
         //intro
         FadeTransition ft = new FadeTransition(Duration.seconds(1), imageView);
         ft.setFromValue(0);
-        ft.setToValue(1);
+        ft.setToValue(0.65);
 
         ParallelTransition parallelTransition = new ParallelTransition(ft);
         parallelTransition.play();
